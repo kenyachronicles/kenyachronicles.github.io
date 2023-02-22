97108f94df357a13aabdafd9d17ec3268344a861
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2023-02-22 17:05:18 +0300

    minor changes

--- thumbnails.py
+++ thumbnails.py
@@ -1,23 +1,27 @@
 source = "assets/images/prologue" 
-dest = source + "/thumbnails"
+dest_small = source + "/small"
+dest_medium = source + "/medium"
 
 from PIL import Image
 import os
 
-files = os.listdir(source)
+files:list[str] = os.listdir(source)
 
 def get_thumbnails(files): 
 
-    if not os.path.exists(dest):
-        os.makedirs(dest)
+    for dest in [dest_small, dest_medium]:
+        if not os.path.exists(dest):
+            os.makedirs(dest)
 
     for filename in files:
         if filename.endswith(".png") or filename.endswith(".jpg"):
             # Load just once, then successively scale down
             im=Image.open(os.path.join(source, filename))
             im = im.convert('RGB')
+            im.thumbnail((640,640))
+            im.save(f"{dest_medium}/{filename}")
             im.thumbnail((140,140))
-            im.save(f"{dest}/{filename}")
+            im.save(f"{dest_small}/{filename}")
      
 
 get_thumbnails(files)
